24cc868253a60e460ad134bd36fb9e7bef28d982
AuKaiHin230004081 <230004081@stu.vtc.edu.hk>
2024-09-30 15:46:39 +0800

    font ui update

--- Assets/Script/CloseUI.cs
+++ Assets/Script/CloseUI.cs
@@ -6,16 +6,19 @@ using UnityEngine.UI;
 public class CloseUI : MonoBehaviour
 {
     public GameObject close;
+    public GameObject closefont;
     // Start is called before the first frame update
     void Start()
     {
         close.gameObject.SetActive(true);
+        closefont.gameObject.SetActive(false);
         GetComponent<Button>().onClick.AddListener(CloseUIOnClick);
     }
 
     public void CloseUIOnClick()
     {
         close.SetActive(false);
+        closefont.gameObject.SetActive(true);
     }
     // Update is called once per frame
 
