db04f243ac03bb999d3cc88bbd757eee39ea5b21
chan kwun ho <161811174+chankwunho@users.noreply.github.com>
2024-10-16 16:42:05 +0800

    itemslot script 16/10

---
+++ Assets/Script/Inventory/Item.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    [SerializeField]
+    private string itemName;
+    [SerializeField]
+    private int quantity;
+    [SerializeField]
+    private Sprite sprite;
+
+    private InventoryManagers inventoryManagers;
+    // Start is called before the first frame update
+    void Start()
+    {
+        inventoryManagers = GameObject.Find("InventoryCanvas").GetComponent<InventoryManagers>();
+    }
+
+
+ private void Oncollision2D (Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            inventoryManagers.AddItem(itemName, quantity, sprite);
+            Destroy(gameObject);
+        }
+    }
