a9da4ed2ce3eec6b484ecb7c627b9debb04c127c
AuKaiHin230004081 <230004081@stu.vtc.edu.hk>
2025-01-20 17:32:43 +0800

    ItemPick( not done yet)

---
+++ Assets/Script/Item/ItemPickManager.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+interface IInteractable
+{
+    public void Interact();
+}
+public class ItemPickManager : MonoBehaviour
+{
+    public Transform InteractorSource;
+    public float InteractRange;
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Ray r = new Ray(InteractorSource.position, InteractorSource.forward);
+            if (Physics.Raycast(r, out RaycastHit hitInfo, InteractRange))
+            {
+               if (hitInfo.collider.gameObject.TryGetComponent(out IInteractable interactObj))
+               {
+                    interactObj.Interact();
+               }
+           }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Item")
+        {
+            Debug.Log("111");
+        }
+    }
+}
+
+
+
+
